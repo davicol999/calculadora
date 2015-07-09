3723bd73b238915e7215adbf938daf0703e6b7e3
Irvito <irvitomark@gmail.com>
2015-07-09 18:07:15 +0200

    calculadora 0.2

--- Main.cpp
+++ Main.cpp
@@ -2,32 +2,38 @@
 
 using namespace std;
 
-    int main()
-    {
-      double x=0;
-      cout << "introduce el primer número";
-      cin>> x;
+int main()
+{
+	double x=0;
+	cout << "introduce el primer número";
+	cin>> x;
       
-      double r2=0;
-      cout << "introduce el segundo número";
-      cin >> r2;
+	double r2=0;
+	cout << "introduce el segundo número";
+	cin >> r2;
       
-      double t=0;
-      cout << "1.sumar"<< endl <<"2.restar"<< endl << "3.multiplicar" << endl << " 4.dividir"<<endl;
-      cout << "¿ que operación quiere hacer?"; 
-      cin >> t;
-      
-      if (t==1){
-	t=x+r2;
-      }
-      if (t==2){
-	t=x-r2;
-      }
-      if (t==3){
-	t=x*r2;
-      }
-      if (t==4){
-	t=x/r2;
-      }
-      cout << " el resultado" << t << endl;
-    }
+	double t=0;
+	int opcion = 0;
+	cout << "1.sumar"<< endl <<"2.restar"<< endl << "3.multiplicar" << endl << " 4.dividir"<<endl;
+	cout << "¿ que operación quiere hacer?"; 
+	cin >> opcion;
+	switch (opcion) {
+		case 1:
+			t=x+r2;
+			break;
+		case 2:
+			t=x-r2;
+			break;
+		case 3:
+			t=x*r2;
+			break;
+		case 4:
+			t=x/r2;
+			break;
+		default:
+			cout << "se ha producido un error"<<endl;
+			return 0;
+	}
+	cout << "resultado"<< t << endl;
+	return 0;
+}
